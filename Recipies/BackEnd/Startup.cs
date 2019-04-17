de186102085777bd42535806a8d5e251ef0b6e44
andriy22 <istep.andriy@gmail.com>
2019-04-17 20:46:03 +0300

    Authorization works [BackEnd]

--- Recipies/BackEnd/Startup.cs
+++ Recipies/BackEnd/Startup.cs
@@ -8,11 +8,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
+using BackEnd.Services.JWT.Models;
+using System.Text;
+using System;
+using Microsoft.AspNetCore.Identity;
+using BackEnd.Services.JWT.Helpers;
+using System.Net;
+using BackEnd.Services.JWT.Auth;
 
 namespace BackEnd
 {
     public class Startup
     {
+        private const string SecretKey = "iNivDmHLpUA223sqsfhqGbMRdRj1PVkH"; //Secure code
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
@@ -27,11 +36,69 @@ namespace BackEnd
               b => b.MigrationsAssembly("BackEnd")));
             services.AddDefaultIdentity<User>().AddEntityFrameworkStores<DatabaseContext>();
 
+            services.AddSingleton<IJwtFactory, JwtFactory>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
             services.AddAutoMapper();
+
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationParameters;
+                configureOptions.SaveToken = true;
+            });
+
+            var builder = services.AddIdentityCore<User>(o =>
+            {
+                // configure identity options
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 6;
+            });
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), builder.Services);
+            builder.AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
+
+            // api user claim policy
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiUser", policy => policy.RequireClaim(Constants.Strings.JwtClaimIdentifiers.Rol, Constants.Strings.JwtClaims.ApiAccess));
+            });
 
             services.AddMvc();
         }
@@ -47,10 +114,22 @@ namespace BackEnd
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+
+            app.UseExceptionHandler(
+                     builder =>
+                     {
+                         builder.Run(
+                        async context =>
+                                 {
+                                     context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                                     context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                                 });
+                     });
+
+            app.UseAuthentication();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
+            app.UseMvc();
 
             app.UseSwagger();
 
