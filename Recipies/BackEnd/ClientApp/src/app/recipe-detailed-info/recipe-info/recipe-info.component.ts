e8ec8b5d904ee69b6287e888f9e5f85a3e05eabe
Олексій Чеботкевич <forest793@ukr.net>
2019-04-17 14:31:07 +0300

    add new component and install primeng

---
+++ Recipies/BackEnd/ClientApp/src/app/recipe-detailed-info/recipe-info/recipe-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-info',
+  templateUrl: './recipe-info.component.html',
+  styleUrls: ['./recipe-info.component.css']
+})
+export class RecipeInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
