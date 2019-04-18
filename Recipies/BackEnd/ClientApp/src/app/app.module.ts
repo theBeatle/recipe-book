dd2259a9e7d22c25eb86f3e2691f1a1b498fef0c
olexiychebotkevich <olexiychebotkevich@A33COMP4>
2019-04-18 17:12:34 +0300

    new changes

--- Recipies/BackEnd/ClientApp/src/app/app.module.ts
+++ Recipies/BackEnd/ClientApp/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
+
+//primeng module
 import {AccordionModule} from 'primeng/accordion';
 import {GalleriaModule} from 'primeng/galleria';
-
+import {RatingModule} from 'primeng/rating';
 
 
 
@@ -15,11 +18,13 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+
 //mycomponents
 import { RecipeInfoComponent } from './recipe-detailed-info/recipe-info/recipe-info.component';
 import { RecipeGalleriaComponent } from './recipe-detailed-info/recipe-galleria/recipe-galleria.component';
 import { RecipeIngradientsComponent } from './recipe-detailed-info/recipe-ingradients/recipe-ingradients.component';
 import { RecipeDirectionsComponent } from './recipe-detailed-info/recipe-directions/recipe-directions.component';
+import { RecipeRaitingComponent } from './recipe-detailed-info/recipe-raiting/recipe-raiting.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +37,8 @@ import { RecipeDirectionsComponent } from './recipe-detailed-info/recipe-directi
     RecipeGalleriaComponent,
     RecipeIngradientsComponent,
     RecipeDirectionsComponent,
+    RecipeRaitingComponent,
+  
     
   ],
   imports: [
@@ -40,6 +47,7 @@ import { RecipeDirectionsComponent } from './recipe-detailed-info/recipe-directi
     FormsModule,
     AccordionModule,
     GalleriaModule,
+    RatingModule,
     
 
     RouterModule.forRoot([
