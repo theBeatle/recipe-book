22a9a0434e89476b3519e4dfc92c06cf18d4c965
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-04-21 13:03:08 +0300

    Switch auth not works

--- Recipies/BackEnd/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ Recipies/BackEnd/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Observable  } from 'rxjs';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,7 +9,18 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
   isExpanded = false;
+  isAuthorized: boolean;
+  constructor(
+    private authenticationService: AuthenticationService,
+  ) { }
 
+  ngOnInit() {
+console.log(this.authenticationService.currentUserValue);
+
+    if (this.authenticationService.currentUserValue) {
+      this.isAuthorized = true;
+    }
+  }
   collapse() {
     this.isExpanded = false;
   }
--- Recipies/BackEnd/ClientApp/src/app/services/authentication.service.ts
+++ Recipies/BackEnd/ClientApp/src/app/services/authentication.service.ts
@@ -38,6 +38,8 @@ export class AuthenticationService {
             }));
     }
 
+
+
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
