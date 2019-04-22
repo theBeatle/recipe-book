1d900d6330db9c2a7c541a97030a236cbc8d6537
andriy22 <istep.andriy@gmail.com>
2019-04-22 22:32:08 +0300

    work

---
+++ Recipies/BackEnd/ClientApp/src/app/services/users.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { HOST_URL } from './../config';
+import { AuthenticationService } from './authentication.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  user: Observable<User>;
+  constructor(private http: HttpClient, private auth: AuthenticationService) { }
+  GetAuthUser() {
+    this.user = this.http.get<User>(HOST_URL + '/api/Users/GetUserById/' + this.auth.currentUserValue.id);
+    return this.user;
+  }
+}
