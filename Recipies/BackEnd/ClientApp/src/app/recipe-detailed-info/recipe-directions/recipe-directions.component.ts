5145c8d26c769263508fb03682697ccfe802806e
Олексій Чеботкевич <forest793@ukr.net>
2019-04-23 23:03:43 +0300

    new changes

--- Recipies/BackEnd/ClientApp/src/app/recipe-detailed-info/recipe-directions/recipe-directions.component.ts
+++ Recipies/BackEnd/ClientApp/src/app/recipe-detailed-info/recipe-directions/recipe-directions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-directions',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDirectionsComponent implements OnInit {
 
+
+  @Input() Directions: string[];
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
--- Recipies/BackEnd/ClientApp/src/app/recipe-detailed-info/recipe-info/recipe-info.component.ts
+++ Recipies/BackEnd/ClientApp/src/app/recipe-detailed-info/recipe-info/recipe-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
-import { RecipeService } from '../recipe.service';
+import { RecipeService } from '../../services/recipe.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,13 +11,14 @@ import { Observable } from 'rxjs';
 export class RecipeInfoComponent implements OnInit {
   
   
-  recipe:Observable<Recipe>;
+  public recipe:Observable<Recipe>;
   constructor() {
-    // private Rs:RecipeService,private RecipeId:string
+    // private Rs:RecipeService,private RecipeId
    }
   
   ngOnInit() {
    //this.recipe=this.Rs.getRecipeById(this.RecipeId);
+   
   }
   
   }
