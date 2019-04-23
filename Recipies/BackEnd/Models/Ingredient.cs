df689b2308885d81e726447e61236620acd16f93
Elizaveta Shemetovska <shemetovska1107@gmail.com>
2019-04-23 14:16:16 +0300

    Add class Ingredient, Vitamin, MicroElement

---
+++ Recipies/BackEnd/Models/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Kkal_100g { get; set; }
+        public double Fats_100g { get; set; }
+        public double Proteins_100g { get; set; }
+        public double Carbohydrates_100g { get; set; }
+        public IEnumerable<Vitamin> Vitamins { get; set; }
+        public IEnumerable<MicroElement> MicroElements { get; set; }
+
+
+
+    }
+}
