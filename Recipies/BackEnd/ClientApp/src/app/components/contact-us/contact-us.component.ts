fb10a879ca3a30e0a56315f44e1b916cda10918c
1 <1@NORMALNY>
2019-04-25 17:32:44 +0300

    done

--- Recipies/BackEnd/ClientApp/src/app/components/contact-us/contact-us.component.ts
+++ Recipies/BackEnd/ClientApp/src/app/components/contact-us/contact-us.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from './Message.service';
+import { Message } from './Message';
+
 
 @Component({
   selector: 'app-contact-us',
@@ -8,10 +12,51 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  messageForm: FormGroup;
+  submitted = false;
+  loading = false;
+  error = '';
+   today = new Date();
+ dd = String(this.today.getDate()).padStart(2, '0');
+ mm = String(this.today.getMonth() + 1).padStart(2, '0'); //January is 0!
+ yyyy = this.today.getFullYear();
+
+  constructor(private fb: FormBuilder,
+     private cS: MessageService,
+     private router: Router,
+      ) { }
 
   ngOnInit() {
-    
-  }
 
+    this.messageForm = this.fb.group({
+      Text: [''],
+      Date: this.yyyy+'-'+this.mm+'-'+this.dd
+      
+    });
+  }
+  onFormSubmit() {
+    this.submitted = true;
+
+   /* if (this.messageForm.invalid) {
+      return;
+    }
+*/
+  this.loading = true;
+    const message = this.messageForm.value;
+    this.CreateUser(message as Message);
+
+  }
+  CreateUser(message: Message) {
+
+      this.cS.createMessage(message).subscribe(
+        () => {
+
+          console.log("OK");
+
+          this.router.navigate(['/']);
+        }
+      );
+
+
+  }
 }
