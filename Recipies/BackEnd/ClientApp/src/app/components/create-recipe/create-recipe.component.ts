2e7bda79bf0869663da0f3f452e7d40e5ca18f9d
andriy22 <istep.andriy@gmail.com>
2019-05-06 23:03:12 +0300

    UI work

--- Recipies/BackEnd/ClientApp/src/app/components/create-recipe/create-recipe.component.ts
+++ Recipies/BackEnd/ClientApp/src/app/components/create-recipe/create-recipe.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RecipeService } from './../../services/recipe.service';
+import { Category } from './../../models/category';
+import { Country } from './../../models/country';
+import { RecipeModel } from './../../models/recipe-model';
 
 @Component({
   selector: 'app-create-recipe',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRecipeComponent implements OnInit {
 
-  constructor() { }
+  recipeForm: FormGroup;
+  CategoryList: Category[];
+  CountryList: Country[];
+  messege: string;
+  constructor(private fb: FormBuilder, private recipe: RecipeService) {
+    this.recipeForm = this.fb.group({
+      topic: ['', Validators.required],
+      description: ['', Validators.required],
+      cookingProcess: ['', Validators.required],
+      category: ['', Validators.required],
+      country: ['', Validators.required],
+    });
+   }
 
   ngOnInit() {
+    this.recipe.getCategories().subscribe((data) => {
+      this.CategoryList = data;
+    });
+    this.recipe.getCountries().subscribe((data) => {
+      this.CountryList = data;
+    });
+  }
+  get f() { return this.recipeForm.controls; }
+  onSubmit() {
+    const model = this.recipeForm.value;
+    this.recipe.CreateRecipe(model as RecipeModel).subscribe(
+    () => {this.messege = 'Recipe Created!'; },
+    () => {this.messege = '400 - BAD REQUEST!'; }
+    );
+    this.recipeForm.reset();
+    if (this.messege != null) {
+        $('#element').toast('show');
+    }
   }
 
 }
