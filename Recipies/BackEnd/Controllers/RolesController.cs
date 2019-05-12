154f9e4bd63981f8343712f01085a4ea8fbce01a
andriy22 <istep.andriy@gmail.com>
2019-05-12 18:37:37 +0300

    add roles bug fix

--- Recipies/BackEnd/Controllers/RolesController.cs
+++ Recipies/BackEnd/Controllers/RolesController.cs
@@ -31,6 +31,17 @@ namespace BackEnd.Controllers
                 return BadRequest(result.Errors);
         }
         [HttpGet]
+        public async Task<IActionResult> IsUserInRole(string uid, string RoleName)
+        {
+            var user = await _userManager.FindByIdAsync(uid);
+            if (user != null)
+            {
+                var res = await _userManager.IsInRoleAsync(user, RoleName);
+                return Ok(res);
+            }
+            return BadRequest("User with uid: " + uid + " not found!");
+        }
+        [HttpGet]
         public async Task<IActionResult> SetUserRole(string uid, string roleName)
         {
             var user = await _userManager.FindByIdAsync(uid);
@@ -48,6 +59,25 @@ namespace BackEnd.Controllers
                 }
             }
             return  BadRequest("User with uid: " + uid + " not found!");
+        }
+        [HttpGet]
+        public async Task<IActionResult> RemoveUserRole(string uid, string roleName)
+        {
+            var user = await _userManager.FindByIdAsync(uid);
+            if (user != null)
+            {
+                IdentityResult result = await this._userManager.RemoveFromRoleAsync(user, roleName);
+
+                if (result.Succeeded)
+                {
+                    return Ok("Ok");
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+            return BadRequest("User with uid: " + uid + " not found!");
         }
         [HttpGet]
         public async Task<IEnumerable<string>> GetRolesByUID(string uid)
